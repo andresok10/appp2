43a42ab450dead8d9bbc631b8b675c6461595f6d
andresok10 <davexgarcia@gmail.com>
2025-08-30 18:56:48 -0500

    ok

--- app.py
+++ app.py
@@ -148,10 +148,10 @@ def descargar():
                 ydl.download([url])
             # return redirect(url_for('serve_download',filename=os.path.basename(filename)))
             #msgx = jsonify(f"{download_type.capitalize()} descargado con éxito como {os.path.basename(filename)}.")
-            msgx = jsonify(f"descargado con éxito como {os.path.basename(filename)}.")
+            #msgx = jsonify(f"descargado con éxito como {os.path.basename(filename)}.")
             #print(msg.data)
-            #msg=os.path.basename(filename)
-            msg = json.loads(msgx.data)
+            msg=os.path.basename(filename)
+            #msg = json.loads(msgx.data)
             #return redirect(url_for("calendario",msg=msg))
             #if msg:
             #return redirect(url_for("serve_download",filename=os.path.basename(filename)))
